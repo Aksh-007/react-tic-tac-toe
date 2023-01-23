3b604488d995bf192aac9f0fdae33021afdcf1d4
Aksh-007 <akshaybarapatre54@gmail.com>
2023-01-23 16:59:23 +0530

    Added game Structure

---
+++ src/components/GameStructure.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Icon from './Icon.js'
+
+import { ToastContainer, toast, Icons } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Card, CardBody, Container, Button, Col, Row } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from 'react';
+
+const itemArray = new Array(9).fill('empty');
+
+const GameStructure = () => {
+    const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("");
+
+  const reloadGame = () => {
+    //
+  }
+
+  const checkWinner = () => {
+
+  }
+
+  const changeButton = itemNumber => {
+
+  }
+  return (
+    <Container className='p-5'>
+    <ToastContainer position='bottom-center' />
+    <Row>
+      <Col md={6} className='offset-md-3'>
+        <div className='grid'>
+          {itemArray.map((item, index) => (
+            <Card>
+              <CardBody className='box'>
+                <Icon/>
+              </CardBody>
+            </Card>
+          ))}
+        </div>
+      </Col>
+    </Row>
+  </Container>
+  )
+}
+
+export default GameStructure
