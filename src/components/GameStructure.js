d5404e48cceebd3de7783dabbcff30e212876378
Aksh-007 <akshaybarapatre54@gmail.com>
2023-01-23 18:34:34 +0530

    aded comments

--- src/components/GameStructure.js
+++ src/components/GameStructure.js
@@ -12,42 +12,63 @@ const itemArray = new Array(9).fill('empty');
 
 const GameStructure = () => {
     const [isCross, setIsCross] = useState(false);
-  const [winMessage, setWinMessage] = useState("");
+    const [winMessage, setWinMessage] = useState("");
 
-  const reloadGame = () => {
-    //in this function we are reloading the game 
-    // so reloading means fixing the state back into original/default position 
-    setIsCross (false);
-    setWinMessage("");
-    itemArray.fill("empty", 0, 9);
+    const reloadGame = () => {
+        setIsCross(false);
+        setWinMessage("");
+        itemArray.fill("empty", 0, 9);
+    };
 
-  }
+    const checkIsWinner = () => {
+        //  checking  winner of the game
+    }
+    const changeItem = itemNumber => {
+        if (winMessage) {
+            return toast(winMessage, { type: "success" });
+        }
 
-  const checkWinner = () => {
+        if (itemArray[itemNumber] === "empty") {
+            itemArray[itemNumber] = isCross ? "cross" : "circle";
+            setIsCross(!isCross);
+        } else {
+            return toast("already filled", { type: "error" });
+        }
 
-  }
+        checkIsWinner();
+    };
 
-  const changeButton = itemNumber => {
-
-  }
-  return (
-    <Container className='p-5'>
-    <ToastContainer position='bottom-center' />
-    <Row>
-      <Col md={6} className='offset-md-3'>
-        <div className='grid'>
-          {itemArray.map((item, index) => (
-            <Card>
-              <CardBody className='box'>
-                <Icon/>
-              </CardBody>
-            </Card>
-          ))}
-        </div>
-      </Col>
-    </Row>
-  </Container>
-  )
-}
-
-export default GameStructure
+    return (
+        <Container className="p-5">
+            <ToastContainer position="bottom-center" />
+            <Row>
+                <Col md={6} className="offset-md-3">
+                    {winMessage ? (
+                        <div className="mb-2 mt-2">
+                            <h1 className="text-success text-uppercase text-center">
+                                {winMessage}
+                            </h1>
+                            <Button color="success" block onClick={reloadGame}>
+                                Reload the game
+                            </Button>
+                        </div>
+                    ) : (
+                        <h1 className="text-center text-warning">
+                            {isCross ? "Cross" : "Circle"} turns
+                        </h1>
+                    )}
+                    <div className="grid">
+                        {itemArray.map((item, index) => (
+                            <Card color="warning" onClick={() => changeItem(index)}>
+                                <CardBody className="box">
+                                    <Icon name={item} />
+                                </CardBody>
+                            </Card>
+                        ))}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+export default GameStructure;
